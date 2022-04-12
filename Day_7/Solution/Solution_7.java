8b32fd441055f6c10d72c7c7c5b5a47c542d0d98
panchalleena1286 <panchalleena8839@gmail.com>
2022-04-12 19:13:45 +0530

    Arrays

---
+++ Day_7/Solution/Solution_7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Solution_7 {
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        in.close();
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[(n - 1) - i] + " ");
+        }
+    }
+
+}
